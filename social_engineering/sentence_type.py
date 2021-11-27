4aa4f78333b502229d31b1e7a1ccef1d56d9d3f8
Shady <ShadyZekry75@gmail.com>
2021-11-27 23:27:47 +0200

    Fix pathes in sentence type detection

--- social_engineering/sentence_type.py
+++ social_engineering/sentence_type.py
@@ -8,14 +8,13 @@ from nltk.parse import stanford
 # Download https://nlp.stanford.edu/software/stanford-corenlp-4.2.0-models-english.jar
 # Open it with Winrar, Extract "edu\stanford\nlp\models\lexparser\englishPCFG.caseless.ser.gz" in the same project directory.
 
-projectPath = 'C:\\Users\\MH\\Documents\\Work\\TextBased-emotion-detection\\social engineering\\'
+projectPath = 'C:\\Users\\MH\\Documents\\Work\\TextBased-emotion-detection\\social_engineering\\'
 os.environ['STANFORD_PARSER'] = projectPath+'jars\\stanford-parser.jar'
-os.environ['STANFORD_MODELS'] = projectPath+'stanford-parser-4.2.0-models.jar'
+os.environ['STANFORD_MODELS'] = projectPath+'jars\\stanford-parser-4.2.0-models.jar'
 os.environ['JAVAHOME'] = 'C:\\Program Files\\AdoptOpenJDK\\jdk-15.0.1.9-hotspot\\bin\\java.exe'
 
 parser = stanford.StanfordParser(model_path=projectPath+"englishPCFG.caseless.ser.gz")
-sentences = parser.raw_parse_sents(
-    ("Hello, My name is Melroy.", "What is your name?"))
+sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your name?","please, reset the router."))
 print(sentences)
 
 # GUI
