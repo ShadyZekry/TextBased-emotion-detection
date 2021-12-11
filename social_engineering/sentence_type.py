f9ae4a58274019a71bfe2d29673255ff4ccf78d6
Shady <ShadyZekry75@gmail.com>
2021-12-11 15:44:01 +0200

    Add tags markdown for stanford parser

--- social_engineering/sentence_type.py
+++ social_engineering/sentence_type.py
@@ -5,3 +5,7 @@ def is_not_question(trees):
             if subtree.label() == '.' and subtree[0]=='?':
                 return False
     return True
+
+# S     => ordinary sentence
+# SQ    => Yes or No question
+# SBARQ => Open questions
