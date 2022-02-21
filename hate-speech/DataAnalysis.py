ded30cdb7eb635b4d6f0965b6bc68bd583c8b647
Shady <ShadyZekry75@gmail.com>
2022-02-21 20:45:17 +0200

    Prepare data analysis main file for hate-speach

--- hate-speech/DataAnalysis.py
+++ hate-speech/DataAnalysis.py
@@ -2,9 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-train_dataset_df = pd.read_csv('./result/OSACT2022-taskA-train.csv', usecols=['tweet','off_label'], encoding='utf-8')
+train_dataset_df = pd.read_csv('./result/OSACT2022-taskA-train.csv', usecols=['tweet','hs_label'], encoding='utf-8')
 
-sns.countplot(x='off_label', data=train_dataset_df).set_title('Off label count plot')
+sns.countplot(x='hs_label', data=train_dataset_df).set_title('Hate speach label count plot')
 
 plt.show()
 
